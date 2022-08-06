bd96ccd0dfb72a5451ec4c3691b19b0f35e25064
Андрей Белоусов <belousov.ban@yandex.ru>
2022-08-06 20:47:51 +0300

    initial commit

---
+++ yatube/posts/admin.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from .models import Post, Group
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = (
+        'pk',
+        'pub_date',
+        'text',        
+        'author',
+        'group',
+    )
+    search_fields = ('text',)
+    list_editable = ('group',)
+    list_filter = ('pub_date',)
+    empty_value_display = '-пусто-'
+
+admin.site.register(Group)
+admin.site.register(Post, PostAdmin)
---
+++ yatube/posts/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import get_object_or_404, render
+from .models import Group, Post
+
+def index(request):
+    posts = Post.objects.select_related('author')[:10]
+    template = 'posts/index.html'
+    context = {'posts': posts}
+    return render(request, template, context)
+
+
+def group_posts(request, slug):
+    group = get_object_or_404(Group, slug=slug)
+    posts = group.posts.select_related('author')[:10]
+    template = 'posts/group_list.html'
+    context = {
+        'group': group,
+        'posts': posts
+    }
+    return render(request, template, context)
+
+# Create your views here.
