569f63f2baad0abb8048c942a096f15d1171c4bc
Андрей Белоусов <belousov.ban@yandex.ru>
2022-08-11 10:48:19 +0300

    файнали

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -1,9 +1,7 @@
-from django.db import models
 from django.contrib.auth import get_user_model
+from django.db import models
 
 User = get_user_model()
-
-# Create your models here.
 
 
 class Group(models.Model):
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import get_object_or_404, render
+
 from .models import Group, Post
 
 
 def index(request):
-    posts = Post.objects.select_related('author')[:10]
-    template = 'posts/index.html'
-    context = {'posts': posts}
-    return render(request, template, context)
+    posts = Post.objects.order_by('-pub_date')[:10]
+    context = {
+        'posts': posts,
+    }
+    return render(request, 'posts/index.html', context)
 
 
 def group_posts(request, slug):
@@ -18,5 +20,3 @@ def group_posts(request, slug):
         'posts': posts
     }
     return render(request, template, context)
-
-# Create your views here.
